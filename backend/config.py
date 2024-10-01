899a8cbbca856c926c76cde866d065a7b98306de
cristina <cristina.trofimov03@gmail.com>
2024-10-01 18:44:37 -0400

    changed up some configurations

--- backend/config.py
+++ backend/config.py
@@ -9,7 +9,7 @@ class ApplicationConfig :
     
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
-    SQLALCHEMY_DATABASE_URI = r"sqlite:///db.sqlite"
+    SQLALCHEMY_DATABASE_URI = r"sqlite:///./db.sqlite"
     
     SESSION_TYPE = "redis"
     SESSION_PERMANENT = False
