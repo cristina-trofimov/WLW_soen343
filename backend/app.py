9f382e06355b28a775b0ce262c0c195106ad86fe
cristina <cristina.trofimov03@gmail.com>
2024-10-02 17:27:34 -0400

    configuration following anothe video without Redis, cookies still not setting

--- backend/app.py
+++ backend/app.py
@@ -1,25 +1,40 @@
-from flask import Flask, request, jsonify, session
+from flask import Flask, jsonify, request, session
 from models import db, Customer
-from config import ApplicationConfig
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
-from flask_session import Session
+from datetime import timedelta
 
 app = Flask(__name__)
-app.config.from_object(ApplicationConfig)
+
+app.config["SECRET_KEY"] = "tamprongs"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///./db.sqlite"
+app.config['SESSION_COOKIE_DOMAIN'] = 'localhost'
+app.config.update(
+    SESSION_COOKIE_SECURE=True,
+    SESSION_COOKIE_HTTPONLY=True,
+    SESSION_COOKIE_SAMESITE='Lax',
+)
+
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_ECHO = True
 
 bcrypt = Bcrypt(app)
-cors = CORS(app, supports_credentials=True)
-server_session = Session(app)
-
+CORS(app, supports_credentials=True, resources={r"/*": {"origins": "http://localhost:5173"}})
 db.init_app(app)
 
 with app.app_context():
     db.create_all()
-    
-    
-@app.route("/@me", methods=["GET"])
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route("/me", methods=["GET"])
 def get_current_customer():
+    
+    print("inside the me method")
+    print("Session contents:", session)
+    print("All cookies:", request.cookies)
     user_id = session.get("user_id")
     
     if not user_id:
@@ -30,20 +45,21 @@ def get_current_customer():
     return jsonify({
         "id" : user.id,
         "email" : user.email}), 200
-      
-    
+
 @app.route("/register", methods=["POST"])
 def register():
+    
     email = request.json["email"]
     password = request.json["password"]
     
-    customer_exists = Customer.query.filter_by(email=email).first() is not None
+    user_exists = Customer.query.filter_by(email=email).first() is not None
     
-    if customer_exists:
-        return jsonify({"error" : "Customer already exists"}), 409
-        
-    hashed_password = bcrypt.generate_password_hash(password)    
+    if user_exists:
+        return jsonify({
+            "error": "User already exists"
+            }), 400
     
+    hashed_password = bcrypt.generate_password_hash(password)
     new_user = Customer(email=email, password=hashed_password)
     db.session.add(new_user)
     db.session.commit()
@@ -51,29 +67,38 @@ def register():
     session["user_id"] = new_user.id
     
     return jsonify({
-        "id" : new_user.id,
-        "email" : new_user.email}), 201
+        "id": new_user.id,
+        "email": new_user.email,
+        })
     
     
-@app.route("/login", methods=["POST"])  
-def login_customer():
+@app.route("/login", methods=["POST"])
+def login():
+    
     email = request.json["email"]
     password = request.json["password"]
     
-    customer = Customer.query.filter_by(email=email).first()
+    user = Customer.query.filter_by(email=email).first()
     
-    if customer is None:
-        return jsonify({"error" : "Invalid email"}), 401
+    if user is None: 
+        return jsonify({
+            "error": "Unauthorised : Incorrect email"
+            }), 401
     
-    if not bcrypt.check_password_hash(customer.password, password):
-        return jsonify({"error" : "Invalid password"}), 401
+    if not bcrypt.check_password_hash(user.password, password):
+        return jsonify({
+            "error": "Unauthorised : Incorrect password"
+            }), 401
+        
+    session["user_id"] = user.id
     
-    session["user_id"] = customer.id
+    print(session)
     
     return jsonify({
-        "id" : customer.id,
-        "email" : customer.email}), 200  
-
+        "id": user.id,
+        "email": user.email,
+        })
+    
+    
 if __name__ == "__main__":
     app.run(debug=True)
-
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,7 +1,29 @@
+import { Button } from "@mantine/core"
+import axiosClient from "../axiosClient"
 
 const HomePage = () => {
+
+
+    const getUser = async () => {
+        try {
+            const response = await axiosClient.get("/me");
+            console.log(response.data);
+        } catch (err) {
+
+            console.error("Error fetching user data:", err);
+        }
+    };
+
+    const handleClick = () => {
+        getUser();
+    };
+
     return (
-        <div>Welcome to HomePage</div>
+        <div>
+            Welcome to HomePage
+
+            <Button onClick={handleClick}>Current User</Button>
+        </div>
     )
 }
 
