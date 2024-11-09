850111cab89a0178787b0431057623ccd8348293
cristina <cristina.trofimov03@gmail.com>
2024-11-08 22:57:53 -0500

    Log in works

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,7 +1,31 @@
+import React, { useState } from 'react'
+import { Button } from '@mantine/core'
+import axiosClient from '../axiosClient'
+import { Customer } from '../interface/customer'
+
 
 const HomePage = () => {
+
+    const [user, setUser] = useState<Customer>()
+
+    const getCurrentUser = async () => {
+        try {
+            const response = await axiosClient.get('/current_user')
+            console.log(response.data)
+            setUser(response.data)
+
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
     return (
-        <div>Welcome to HomePage</div>
+        <><div>Welcome to HomePage</div>
+            <Button onClick={getCurrentUser}>Me</Button>
+            {user && <div>{user.email}</div>}
+
+        </>
+
     )
 }
 
